e92ebe3beef6f78421bf8eec282c5d48626e9536
darrior <andrei150202@gmail.com>
2022-09-29 00:19:51 +0300

    readme edit

--- README.md
+++ README.md
@@ -4,14 +4,17 @@ with possibillity of setting due dates, changing states and other.
 
 Bot writen in Rust with Teloxide framework.
 ## Building
+Clone:
+```
+$ git clone https://github.com/Silsword/aaa-bot
+$ cd aaa-bot
+```
 Add your token to this line in main.rs:
 ```rust
 let bot = Bot::new("<token here>").auto_send();
 ```
-
+Build:
 ```
-$ git clone https://github.com/Silsword/aaa-bot
-$ cd aaa-bot
 $ cargo build --release
 ```
 ## Usage
--- src/file.rs
+++ src/file.rs
@@ -1,25 +1,25 @@
 //! Work with files
 use std::{fs::File, io::{Write, Read}};
-use super::notes::Notes;
+use super::todo_list::ToDoList;
 
 const SAVE : &str = "notes.json";
 
 /// Save to existing file or create new
-pub fn save_to_file(notes : &Notes) {
+pub fn save_to_file(notes : &ToDoList) {
     let mut save = File::create(SAVE).expect("Can not open or file");
     save.write_all(notes.to_json().as_bytes()).expect("Can not write to file");
 }
 
 /// Load from existing file or initialize new `Notes` struct
-pub fn load_from_file() -> Notes {
+pub fn load_from_file() -> ToDoList {
     if let Ok(mut load) = File::open(SAVE) {
 	let mut json = String::new();
 	if let Ok(_) = load.read_to_string(&mut json) {
-	    Notes::from_json(json)
+	    ToDoList::from_json(json)
 	} else {
-	    Notes::new()
+	    ToDoList::new()
 	}
     } else {
-	Notes::new()
+	ToDoList::new()
     }
 }
